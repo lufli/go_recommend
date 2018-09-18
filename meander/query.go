ca46f14883f9aa1d6b85d72c5bc646b04b6fc71c
Lufan Li <lufan@pulsedata.io>
2018-09-18 17:54:21 -0400

    half way

--- meander/query.go
+++ meander/query.go
@@ -1,3 +1,48 @@
 package meander
 
-type Place struct {}
+type Place struct {
+	*googleGeometry `json:"geometry"`
+	Name            string         `json:"name"`
+	Icon            string         `json:"icon"`
+	Photos          []*googlePhoto `json:"photos"`
+	Vicinity        string         `json:"vicinity"`
+}
+
+type Query struct {
+	Lat          float64
+	Lng          float64
+	Journey      []string
+	Radius       int
+	CostRangeStr string
+}
+
+type googleResponse struct {
+	Result []*Place `json:"results"`
+}
+
+type googleGeometry struct {
+	Lat float64 `json:"lat"`
+	Lng float64 `json:"lng"`
+}
+
+type googlePhoto struct {
+	PhotoRef string `json:"photo_reference"`
+	URL      string `json:"url"`
+}
+
+func (p *Place) Public() interface{} {
+	return map[string]interface{}{
+		"name":     p.Name,
+		"icon":     p.Icon,
+		"photos":   p.Photos,
+		"vicinity": p.Vicinity,
+		"lat":      p.Lat,
+		"lng":      p.Lng,
+	}
+}
+
+var APIKey string
+
+func (q *Query) find(types string) (*googleResponse, error) {
+	u := "https://maps.googleapis.com/maps/api/place/nearbysearch/json"
+}
